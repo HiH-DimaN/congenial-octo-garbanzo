487ef374bbd1d6cbd0349ce5c4735db7d550b467
HiH-DimaN <hih.diman@gmail.com>
2024-07-12 13:36:19 +0300

    add new project

--- Gitpractice.sol
+++ Gitpractice.sol
@@ -6,7 +6,7 @@ pragma solidity ^0.8.0;
 contract Adder {
 
     // Function for calculating the sum of two numbers
-    function add(uint256 a, uint256 b) public pure returns(uint256) {
-        return a + b;
+    function add(uint256 a, uint256 c) public pure returns(uint256) {
+        return a + c;
     }
 }

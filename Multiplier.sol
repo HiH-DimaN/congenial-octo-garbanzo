a4fe1e1565c17a874c2d3da8c2f3c92ae89ce4e0
HiH_DimaN <163323591+HiH-DimaN@users.noreply.github.com>
2024-07-12 14:52:37 +0300

    Update Multiplier.sol

--- Multiplier.sol
+++ Multiplier.sol
@@ -1,13 +1,23 @@
-// Task 9
-// Contract for multiplication 
-// Write a contract that will calculate the product of two numbers passed to a function.
+/** Task 9
+Contract for multiplication 
+Write a contract that will calculate the product of two numbers passed to a function.
+*/
+
+/// @author HiH_DimaN
+/// @notice Contract for calculating
+
 
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
 
-// Contract for calculating the product of two numbers
+/** @title Contract for calculating the product of two numbers. */
 contract Multiplier {
+    /** @dev Calculating multiplication of two variables.
+      * @param a Variable.
+      * @param b Variablee.
+      * @return a * b The сalculating multiplication.
+      */
 
     // Function for calculating the product of two numbers
     function multiply(uint256 a, uint256 b) public pure returns (uint256) {
